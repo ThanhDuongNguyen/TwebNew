9066f6b8620173bc00eb5143185084765fbb5615
Thanh Duong <ntduong151099@gmail.com>
2021-02-23 15:32:02 +0700

    Add project files.

---
+++ TwebNew/Extentions/TwebBuilderExtensions.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Options;
+using OrchardCore.Data;
+using OrchardCore.Data.Migration;
+using OrchardCore.Environment.Shell;
+using OrchardCore.Environment.Shell.Models;
+using OrchardCore.Environment.Shell.Scope;
+using OrchardCore.Modules;
+using YesSql;
+using YesSql.Indexes;
+using YesSql.Provider.MySql;
+using YesSql.Provider.PostgreSql;
+using YesSql.Provider.Sqlite;
+using YesSql.Provider.SqlServer;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class TwebBuilderExtensions
+    {
+        /// <summary>
+        /// Adds tenant level data access services.
+        /// </summary>
+        /// <param name="builder">The <see cref="OrchardCoreBuilder"/>.</param>
+        public static OrchardCoreBuilder AddDataAccess(this OrchardCoreBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped<IDataMigrationManager, DataMigrationManager>();
+                services.AddScoped<IModularTenantEvents, AutomaticDataMigrations>();
+
+                // Adding supported databases
+                services.TryAddDataProvider(name: "Sql Server", value: "SqlConnection", hasConnectionString: true, sampleConnectionString: "Server=localhost;Database=Orchard;User Id=username;Password=password", hasTablePrefix: true, isDefault: false);
+                services.TryAddDataProvider(name: "Sqlite", value: "Sqlite", hasConnectionString: false, hasTablePrefix: false, isDefault: true);
+                services.TryAddDataProvider(name: "MySql", value: "MySql", hasConnectionString: true, sampleConnectionString: "Server=localhost;Database=Orchard;Uid=username;Pwd=password", hasTablePrefix: true, isDefault: false);
+                services.TryAddDataProvider(name: "Postgres", value: "Postgres", hasConnectionString: true, sampleConnectionString: "Server=localhost;Port=5432;Database=Orchard;User Id=username;Password=password", hasTablePrefix: true, isDefault: false);
+
+                // Configuring data access
+
+                services.AddSingleton<IStore>(sp =>
+                {
+                    var shellSettings = sp.GetService<ShellSettings>();
+
+                    // Before the setup a 'DatabaseProvider' may be configured without a required 'ConnectionString'.
+                    if (shellSettings.State == TenantState.Uninitialized || shellSettings["DatabaseProvider"] == null)
+                    {
+                        return null;
+                    }
+
+                    IConfiguration storeConfiguration = new YesSql.Configuration();
+
+                    switch (shellSettings["DatabaseProvider"])
+                    {
+                        case "SqlConnection":
+                            const string Db = "database";
+                            const string Catalog = "Initial Catalog";
+                            var dbname = "";
+                            var indexdbName = 0;
+                            var indexCatalog = 0;
+                            var index = 0;
+
+                            indexdbName = shellSettings["ConnectionString"].IndexOf(Db);
+                            indexCatalog = shellSettings["ConnectionString"].IndexOf(Catalog);
+
+                            if (indexCatalog != -1)
+                            {
+                                indexCatalog = indexCatalog + Catalog.Length;
+                                index = indexCatalog;
+                            }
+                            else
+                            {
+                                indexdbName = indexdbName + Db.Length;
+                                index = indexdbName;
+                            }
+
+                            dbname = shellSettings["ConnectionString"].Substring(index).Trim();
+                            dbname = dbname.Substring(dbname.IndexOf("=") + 1).Trim();
+                            dbname = dbname.Substring(0, dbname.IndexOf(";")).Trim();
+
+                            shellSettings["ConnectionString"] = shellSettings["ConnectionString"].Replace(dbname, "master");
+
+                            storeConfiguration.SetNewDatabaseName(dbname);
+                            storeConfiguration
+                                .UseSqlServer(shellSettings["ConnectionString"])
+                                .UseBlockIdGenerator();
+
+                            StoreFactory.CreateDatabaseAsync(storeConfiguration).GetAwaiter().GetResult();
+                            dbname = dbname.Replace("[", "");
+                            dbname = dbname.Replace("]", "");
+
+                            shellSettings["ConnectionString"] = shellSettings["ConnectionString"].Replace("master", dbname);
+
+                            storeConfiguration
+                                        .UseSqlServer(shellSettings["ConnectionString"])
+                                        .UseBlockIdGenerator();
+                            break;
+                        case "Sqlite":
+                            var shellOptions = sp.GetService<IOptions<ShellOptions>>();
+                            var option = shellOptions.Value;
+                            var databaseFolder = Path.Combine(option.ShellsApplicationDataPath, option.ShellsContainerName, shellSettings.Name);
+                            var databaseFile = Path.Combine(databaseFolder, "yessql.db");
+                            Directory.CreateDirectory(databaseFolder);
+                            storeConfiguration
+                                .UseSqLite($"Data Source={databaseFile};Cache=Shared", IsolationLevel.ReadUncommitted)
+                                .UseDefaultIdGenerator();
+                            break;
+                        case "MySql":
+                            storeConfiguration
+                                .UseMySql(shellSettings["ConnectionString"], IsolationLevel.ReadUncommitted)
+                                .UseBlockIdGenerator();
+                            break;
+                        case "Postgres":
+                            storeConfiguration
+                                .UsePostgreSql(shellSettings["ConnectionString"], IsolationLevel.ReadUncommitted)
+                                .UseBlockIdGenerator();
+                            break;
+                        default:
+                            throw new ArgumentException("Unknown database provider: " + shellSettings["DatabaseProvider"]);
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(shellSettings["TablePrefix"]))
+                    {
+                        storeConfiguration = storeConfiguration.SetTablePrefix(shellSettings["TablePrefix"] + "_");
+                    }
+
+                    var store = StoreFactory.CreateAsync(storeConfiguration).GetAwaiter().GetResult();
+                    var indexes = sp.GetServices<IIndexProvider>();
+
+                    store.RegisterIndexes(indexes);
+
+                    return store;
+                });
+
+                services.AddScoped(sp =>
+                {
+                    var store = sp.GetService<IStore>();
+
+                    if (store == null)
+                    {
+                        return null;
+                    }
+
+                    var session = store.CreateSession();
+
+                    var scopedServices = sp.GetServices<IScopedIndexProvider>();
+
+                    session.RegisterIndexes(scopedServices.ToArray());
+
+                    ShellScope.RegisterBeforeDispose(scope =>
+                    {
+                        return session.CommitAsync();
+                    });
+
+                    return session;
+                });
+
+                services.AddScoped<ISessionHelper, SessionHelper>();
+
+                services.AddTransient<IDbConnectionAccessor, DbConnectionAccessor>();
+            });
+
+            return builder;
+        }
+    }
+}
---
+++ TwebNew/Extentions/TwebServiceExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.ResourceManagement.TagHelpers;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class TwebServiceExtensions
+    {
+        /// <summary>
+        /// Adds CMS services to the application.
+        /// </summary>
+        public static OrchardCoreBuilder AddTwebCms(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            var builder = services.AddOrchardCore()
+                .AddCommands()
+                .AddMvc()
+                .AddEmailAddressValidator()
+                .AddSetupFeatures("OrchardCore.Setup")
+
+                .AddDataAccess()
+                .AddDataStorage()
+                .AddBackgroundService()
+
+                .AddTheming()
+                .AddLiquidViews()
+                .AddCaching();
+
+            // OrchardCoreBuilder is not available in OrchardCore.ResourceManagement as it has to
+            // remain independent from OrchardCore.
+            builder.ConfigureServices(s =>
+            {
+                s.AddResourceManagement();
+
+                s.AddTagHelpers<LinkTagHelper>();
+                s.AddTagHelpers<MetaTagHelper>();
+                s.AddTagHelpers<ResourcesTagHelper>();
+                s.AddTagHelpers<ScriptTagHelper>();
+                s.AddTagHelpers<StyleTagHelper>();
+            });
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds Orchard CMS services to the application and let the app change the
+        /// default tenant behavior and set of features through a configure action.
+        /// </summary>
+        public static IServiceCollection AddOrchardCms(this IServiceCollection services, Action<OrchardCoreBuilder> configure)
+        {
+            var builder = services.AddOrchardCms();
+
+            configure?.Invoke(builder);
+
+            return services;
+        }
+    }
+}
